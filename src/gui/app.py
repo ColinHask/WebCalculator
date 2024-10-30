383a312244000741d41a0763ae853b78cbd31b76
ColinHask <colinhaskins2022@gmail.com>
2024-10-30 19:24:34 -0400

    progress, not done

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from src import logic
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
+    # initially render html
     return render_template('index.html')
+
+@app.route('/calculate', methods = ['POST'])
+def calculate():
+    # get entry value
+    entry = request.form.get('entry')
+
+    #render template with entry value submitted
+    return render_template('index.html', entry=entry)
