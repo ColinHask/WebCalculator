5ccf1ee7528cde15cfeb365d060ee6c2d0ded9d2
M5-sich <m5.michael.sich@gmail.com>
2024-10-30 00:18:20 -0400

    Organized some of the files and also figured out how to obtain 100% coverage
    Alot of the errors stemed from python not being accurate aver 10 decimal points so i just rounded the values to 10. Also there was a division by zero error and an error where the user doesnt enter anything and I fixed those with try and catch statments (in python its called try and except but its essentially the same thing). Finally I renamed all the methods to make sure its concise and not overcomplicated. Let me know what you think. Also maybe we need to do a try and except for all methods.

--- src/regression.py
+++ src/regression.py
@@ -3,30 +3,40 @@ import math
 
 def linear_regression(xy_list):
     number_of_pairs = len(xy_list)
+    try:
+        total_x = 0
+        for i in xy_list:
+            total_x += i[0]
+        total_y = 0
+        for i in xy_list:
+            total_y += i[1]
 
-    total_x = 0
-    for i in xy_list:
-        total_x += i[0]
-    average_x = total_x / number_of_pairs
+        average_x = total_x / number_of_pairs
+        average_y = total_y / number_of_pairs
 
-    total_y = 0
-    for i in xy_list:
-        total_y += i[1]
-    average_y = total_y / number_of_pairs
+        numerator_sum = 0
+        denominator_sum = 0
+        for i in xy_list:
+            x_difference = i[0] - average_x
+            y_difference = i[1] - average_y
 
-    xy_difference_product = 0
-    x_difference_squared = 0
-    for i in xy_list:
-        xy_difference_product += (i[0] - average_x) * (i[1] - average_y)
-        x_difference_squared += (i[0] - average_x) ** 2
-    m = xy_difference_product / x_difference_squared
+            numerator_sum += x_difference * y_difference
+            denominator_sum += x_difference ** 2
 
-    b = average_y - m * average_x
+        estimated_slope = numerator_sum / denominator_sum
+        y_intercept = average_y - estimated_slope * average_x
 
-    print(f"y = {m}x + {b}")
+        return estimated_slope, y_intercept
 
+    except ZeroDivisionError:
+        raise ValueError("you cannot divide by zero")
 
 def predict_y(x, m, b):
-    # y = mx + b
-    predicted_y = (m * x) + b
-    return predicted_y
+    try:
+        # y = mx + b
+        predicted_y = (m * x) + b
+        return predicted_y
+
+    except TypeError:
+        raise ValueError("all user inputs have to be rational numbers")
+
--- src/test_logic.py
+++ src/test_logic.py
@@ -25,8 +25,6 @@ def test_calculate_mean_empty_list():
         mean([])
 
 
-
-
 def test_standard_deviation():
     result = standard_deviation([1, 2, 3, 4, 5])
     assert result == 1.4142135623730951
@@ -35,8 +33,6 @@ def test_standard_deviation():
 def test_standard_deviation_empty_list():
     with pytest.raises(ValueError):
         standard_deviation([])
-
-
 
 
 def test_z_score():
@@ -56,14 +52,16 @@ def test_linear_regression():
     test_list = [(1.47, 52.21), (1.5, 53.12), (1.52, 54.48), (1.55, 55.84), (1.57, 57.2), (1.6, 58.57), (1.63, 59.93),
                  (1.65, 61.29), (1.68, 63.11), (1.7, 64.47), (1.73, 66.28), (1.75, 68.1), (1.78, 69.92), (1.8, 72.19),
                  (1.83, 74.46)]
-    m_slope, b_intercept = linear_regression(test_list)
-    assert m_slope == 61.272186542107434
-    assert b_intercept == -39.061955918838656
+
+    expected_slope, expected_y_intercept = linear_regression(test_list)
+
+    assert round(expected_slope, 10) == round(61.272186542107434, 10)
+    assert round(expected_y_intercept, 10) == round(-39.061955918838656, 10)
+
 
 def test_linear_regression_empty_list():
     with pytest.raises(ValueError):
         linear_regression([])
-
 
 
 def test_predict_y():
@@ -74,7 +72,7 @@ def test_predict_y():
     result = predict_y(x_given_value, m_slope, b_intercept)
     assert result == 54.990850423296244
 
+
 def test_predict_y_empty_list():
     with pytest.raises(ValueError):
-        predict_y([])
-
+        predict_y(None, 61.272186542107434, -39.061955918838656)
