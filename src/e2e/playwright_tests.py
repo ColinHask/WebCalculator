40010d46a9d42d50c76757944ea2aab5ce16bcef
ColinHask <colinhaskins2022@gmail.com>
2024-12-02 00:25:24 -0500

    added all 8 playwright tests, all pass

--- src/e2e/playwright_tests.py
+++ src/e2e/playwright_tests.py
@@ -77,6 +77,20 @@ def test_MeanUi_NumberInput_CorrectResult(page: Page):
 
 # preq-E2E-TEST-10	From the application's default state, compute the z-score for 5.5 with a mean of 7 and a standard deviation of 3.060787652326. Verify that the result is -0.49007.
 
+def test_ZScoreUi_NumberInput_CorrectResult(page: Page):
+    #arrange
+    expected_value = "-0.490069"
+
+    #act
+    #fill textbox, click button, then find output box (msgBoard)
+    page.goto("http://127.0.0.1:5000")
+    page.get_by_role("textbox").fill("5.5, 7, 3.060787652326")
+    page.get_by_role("button", name="Compute Z Score | value, mean, stdDev on one line").click()
+    msgboard = page.locator("#MsgBoard")
+
+    #assert
+    expect(msgboard).to_have_text(re.compile(expected_value))
+
 # preq-E2E-TEST-11	From the application's default state, compute the single linear regression formula for the following 12 X,Y pairs. Verify the result is y = -0.04596X + 6.9336.
 # 5,3
 # 3,2
@@ -91,4 +105,45 @@ def test_MeanUi_NumberInput_CorrectResult(page: Page):
 # 12,17
 # 3,-1
 
+def test_LinearRegressionUi_XYInput_CorrectResult(page: Page):
+    #arrange
+    expected_value = "y = -0.0459615329x + 6.9335877814"
+
+    #act
+    #fill textbox, click button, then find output box (msgBoard)
+    page.goto("http://127.0.0.1:5000")
+    page.get_by_role("textbox").fill("""5,3
+3,2
+2,15
+1,12.3
+7.5,-3
+4,5
+3,17
+4,3
+6.42,4
+34,5
+12,17
+3,-1""")
+
+    page.get_by_role("button", name="Compute Single Linear Regression Formula | one x,y pair per line").click()
+    msgboard = page.locator("#MsgBoard")
+
+
+    #assert
+    expect(msgboard).to_contain_text(expected_value)
+
 # preq-E2E-TEST-12	From the application's default state, predict the Y value for the regression values X=6, M=-0.04596, B=6.9336. Verify the result is 6.65784.
+
+def test_PredictYUi_NumberInput_CorrectResult(page: Page):
+    #arrange
+    expected_value = "6.65784"
+
+    #act
+    #fill textbox, click button, then find output box (msgBoard)
+    page.goto("http://127.0.0.1:5000")
+    page.get_by_role("textbox").fill("6, -0.04596, 6.9336")
+    page.get_by_role("button", name="Predict Y From Linear Regression Formula | x, m, b on one line").click()
+    msgboard = page.locator("#MsgBoard")
+
+    #assert
+    expect(msgboard).to_have_text(re.compile(expected_value))
