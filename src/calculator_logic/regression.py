5de47aeb49a022c1d0c16a680fa43cfbd1bfea16
M5-sich <m5.michael.sich@gmail.com>
2024-12-02 17:48:12 -0500

    no try catch update

--- src/calculator_logic/regression.py
+++ src/calculator_logic/regression.py
@@ -11,51 +11,49 @@ def linear_regression(xy_list):
     if number_of_pairs < 2:
         raise ValueError("Must have at least two inputs.")
 
-    try:
-        # summing up all the x elements in each pair and storing it in a total_x value
-        total_x = 0
-        for i in xy_list:
-            total_x += i[0]
+    
+    # summing up all the x elements in each pair and storing it in a total_x value
+    total_x = 0
+    for i in xy_list:
+        total_x += i[0]
 
-        # summing up all the y elements in each pair and storing it in a total_y value
-        total_y = 0
-        for i in xy_list:
-            total_y += i[1]
+    # summing up all the y elements in each pair and storing it in a total_y value
+    total_y = 0
+    for i in xy_list:
+        total_y += i[1]
 
-        # obtaining the average for both elements in pairs
-        average_x = total_x / number_of_pairs
-        average_y = total_y / number_of_pairs
+    # obtaining the average for both elements in pairs
+    average_x = total_x / number_of_pairs
+    average_y = total_y / number_of_pairs
 
-        # the formula for estimating slope is a big fraction. For clarity, the calculations for numerator are stored
-        # in a numerator variable, and the calculations for denominator are stored in a denominator variable.
-        numerator_sum = 0
-        denominator_sum = 0
-        for i in xy_list:
-            # each x and y element in each pair has its average subtracted from it
-            x_difference = i[0] - average_x
-            y_difference = i[1] - average_y
-            # total numerator and denominator sum up to a result by the end of the loop
-            numerator_sum += x_difference * y_difference
-            denominator_sum += x_difference ** 2
+    # the formula for estimating slope is a big fraction. For clarity, the calculations for numerator are stored
+    # in a numerator variable, and the calculations for denominator are stored in a denominator variable.
+    numerator_sum = 0
+    denominator_sum = 0
+    for i in xy_list:
+        # each x and y element in each pair has its average subtracted from it
+        x_difference = i[0] - average_x
+        y_difference = i[1] - average_y
+        # total numerator and denominator sum up to a result by the end of the loop
+        numerator_sum += x_difference * y_difference
+        denominator_sum += x_difference ** 2
 
-        # identical x value error
-        if denominator_sum == 0:
-            raise ValueError("Linear regression is not defined as all x-values are identical.")
+    # identical x value error
+    if denominator_sum == 0:
+        raise ValueError("Linear regression is not defined as all x-values are identical.")
 
-        # slope is calculated from numerator and denominator calculations
-        estimated_slope = numerator_sum / denominator_sum
+    # slope is calculated from numerator and denominator calculations
+    estimated_slope = numerator_sum / denominator_sum
 
-        # using slope equation, the y intercept is calculated
-        y_intercept = average_y - estimated_slope * average_x
+    # using slope equation, the y intercept is calculated
+    y_intercept = average_y - estimated_slope * average_x
 
-        # returning the equation as a string with rounded values
-        equation = f"y = {round(estimated_slope, 10)}x + {round(y_intercept, 10)}"
+    # returning the equation as a string with rounded values
+    equation = f"y = {round(estimated_slope, 10)}x + {round(y_intercept, 10)}"
 
-        return equation
+    return equation
 
-    except ZeroDivisionError:
-        # checking for division by zero exception
-        raise ValueError("you cannot divide by zero")
+
 
 def predict_y(x, m, b):
     """
