5075e162c788ce632455aecfecfe72a917889a41
M5-sich <m5.michael.sich@gmail.com>
2024-12-02 19:35:07 -0500

    Formating 79 character

--- src/calculator_logic/regression.py
+++ src/calculator_logic/regression.py
@@ -1,10 +1,12 @@
 import math
 
+###############################################################################
 
 def linear_regression(xy_list):
     """
-    Intakes a list of number pairs (e.g., [(x1,y1),...,(xn,yn)]) and returns the estimated slope and
-    then uses the calculations of that slope to return the predicted y intercept
+    Intakes a list of number pairs (e.g., [(x1,y1),...,(xn,yn)]) and returns
+    the estimated slope and then uses the calculations of that slope to return
+    the predicted y intercept
     """
 
     number_of_pairs = len(xy_list)
@@ -12,12 +14,14 @@ def linear_regression(xy_list):
         raise ValueError("Must have at least two inputs.")
 
     
-    # summing up all the x elements in each pair and storing it in a total_x value
+    # summing up all the x elements in each pair and storing it in a
+    # total_x value
     total_x = 0
     for i in xy_list:
         total_x += i[0]
 
-    # summing up all the y elements in each pair and storing it in a total_y value
+    # summing up all the y elements in each pair and storing it in a
+    # total_y value
     total_y = 0
     for i in xy_list:
         total_y += i[1]
@@ -26,21 +30,26 @@ def linear_regression(xy_list):
     average_x = total_x / number_of_pairs
     average_y = total_y / number_of_pairs
 
-    # the formula for estimating slope is a big fraction. For clarity, the calculations for numerator are stored
-    # in a numerator variable, and the calculations for denominator are stored in a denominator variable.
+    # the formula for estimating slope is a big fraction. For clarity,
+    # the calculations for numerator are stored in a numerator variable,
+    # and the calculations for denominator are stored in a denominator
+    # variable.
     numerator_sum = 0
     denominator_sum = 0
     for i in xy_list:
-        # each x and y element in each pair has its average subtracted from it
+        # each x and y element in each pair has its average subtracted
+        # from it
         x_difference = i[0] - average_x
         y_difference = i[1] - average_y
-        # total numerator and denominator sum up to a result by the end of the loop
+        # total numerator and denominator sum up to a result by the end
+        # of the loop
         numerator_sum += x_difference * y_difference
         denominator_sum += x_difference ** 2
 
     # identical x value error
     if denominator_sum == 0:
-        raise ValueError("Linear regression is not defined as all x-values are identical.")
+        raise ValueError("Linear regression is not defined as all "
+                         "x-values are identical.")
 
     # slope is calculated from numerator and denominator calculations
     estimated_slope = numerator_sum / denominator_sum
@@ -53,13 +62,12 @@ def linear_regression(xy_list):
 
     return equation
 
-
-
 def predict_y(x, m, b):
     """
-    intakes three variables which I purposely named x, m and b since this calculation using form y = mx + b standard
-    linear polynomial equation. Finally, it returns the value of the function (i.e., y) using the slope, the x value
-    and the y intercept.
+    intakes three variables which I purposely named x, m and b since this
+    calculation using form y = mx + b standard linear polynomial equation.
+    Finally, it returns the value of the function (i.e., y) using the slope,
+    the x value and the y intercept.
     """
     try:
         predicted_y = (m * x) + b
--- src/calculator_logic/statistics.py
+++ src/calculator_logic/statistics.py
@@ -1,14 +1,13 @@
 import math
 
+###############################################################################
 
 def mean(number_list):
     """
-    Takes a list of any real number elements and then calculates the mean by adding all the elements and dividing by
-    the number of them
+    Takes a list of any real number elements and then calculates the mean by
+    adding all the elements and dividing by the number of them
     """
-
     # checking for empty list condition
-
     if len(number_list) == 0:
         raise ValueError
     else:
@@ -19,15 +18,17 @@ def mean(number_list):
             ans += number
         return ans / count
 
-
 def sample_standard_deviation(number_list):
     """
     Intakes a list of any numbers and calculates the sample standard deviation.
-    Instead of using n like with population deviation, variance is calculated using n-1.
+    Instead of using n like with population deviation, variance is calculated
+    using n-1.
     """
-    # if statement that accounts for 0 and 1 since n-1 results in negative values
+    # if statement that accounts for 0 and 1 since n-1 results in negative
+    # values
     if len(number_list) < 2:
-        raise ValueError("At least two data points are needed for sample standard deviation.")
+        raise ValueError("At least two data points are needed for sample"
+                         " standard deviation.")
 
     avg = mean(number_list)
 
@@ -41,19 +42,19 @@ def sample_standard_deviation(number_list):
 
     return sample_deviation
 
-
 def population_standard_deviation(number_list):
     """
-    Intakes a list of any numbers and calculates the mean from the mean function.
-    The mean is then used in the variance function. Finally, the square root of the variance
-    give the standard deviation
+    Intakes a list of any numbers and calculates the mean from the mean
+    function. The mean is then used in the variance function. Finally, the
+    square root of the variance give the standard deviation
     """
     # checking for empty list condition
     if len(number_list) == 0:
         raise ValueError
 
     else:
-        # using mean function defined above and then naming the variable avg for clarity
+        # using mean function defined above and then naming the variable
+        # avg for clarity
         avg = mean(number_list)
         total = 0
         for i in number_list:
@@ -68,8 +69,10 @@ def population_standard_deviation(number_list):
 
 def z_score(value, average, variation):
     """
-    Takes three inputs value, average (i.e., mean), and variation (i.e., standard deviation) and then returns the z score.
-    Chose a different synonym for mean and standard deviation due to methods having similar names
+    Takes three inputs value, average (i.e., mean), and variation
+    (i.e., standard deviation) and then returns the z score. Chose a
+    different synonym for mean and standard deviation due to methods
+    having similar names
     """
     if variation == 0:
         raise ValueError
